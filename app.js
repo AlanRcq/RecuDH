e5b87376c72015c083a268d91ee00bd82ceacc80
AlanRcq <acueto@fi.uba.ar>
2022-08-27 11:40:51 -0300

    Scaffold 2

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.listen(3000, () => console.log("Puerto 3001 funcionando correctamente"));
+app.listen(3000, () => console.log("Puerto 3000 funcionando correctamente"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +15,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
